fb60181b246f6b7a7cf4a5d2886091131d643b41
gcrosse <geoffrey.crosse@gmail.com>
2025-04-19 16:28:24 -0400

    added explantory comments to clarify logic in classes and methods

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// BASE CLASS FOR PRODUCTS
+
 class ProductProperties {
     constructor(name, price, quantity) {
         this.name = name;
@@ -10,14 +13,14 @@ class ProductProperties {
     toString() {
         return `Product: ${this.name},  Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
     }
-
+    // STATIC METHOD TO APPLY DISCOUNT
     static applyDiscount(product, discount) {
         product.forEach(product => {
             product.price = product.price * (1 - discount);
         });
     }
 }
-
+//SUBCLASS FOR PREISHABLE PRODUCTS
 class PerishableProductProperties extends ProductProperties {
     constructor(name, price, quantity, expirationDate) {
         super(name, price, quantity);
@@ -42,28 +45,36 @@ class Store {
         return this.inventory.find(product => product.name.toLowerCase() === name.toLowerCase())
     }
 }
-
+//CREATE STORE INSTANCES
 const myStore = new Store();
+
+//CREATE PRODUCTS
 
 const product1 = new ProductProperties("Notebook", 3.50, 40);
 const product2 = new ProductProperties("Pen", 1.00, 100);
 const product3 = new ProductProperties("Stapler", 5.25, 15);
+//CREATE PREISHABLE PRODUCTS
 
 const perishable1 = new PerishableProductProperties("Milk", 2.50, 10, "2025-04-27");
 const perishable2 = new PerishableProductProperties("Cheese", 4.0, 5, "2025-04-28");
 
+//ADD PRODUCTS TO STORE
 myStore.addProduct(product1);
 myStore.addProduct(product2);
 myStore.addProduct(product3);
 myStore.addProduct(perishable1);
 myStore.addProduct(perishable2);
 
+//PRINT TOTAL INVENTORY VALUE FOR DISCOUNT
 console.log(" Inventory Value Before Discount: $" + myStore.getInventoryValue().toFixed(2));
 
+//APPLY 15% DISCOUNT
 ProductProperties.applyDiscount(myStore.inventory, 0.15);
 
+//PRINT TOTAL INVENTORY VALUE AFTER DISCOUNT
 console.log(" Inventory Value After 15% Discount: $" + myStore.getInventoryValue().toFixed(2));
 
+//FIND A PRODUCT BY NAME
 const searchedProduct = myStore.findProductByName("Milk");
 if (searchedProduct) {
     console.log ("Product Found: " + searchedProduct.toString());
